b56f3f7878584ce7562b1975a60fa917febfbbde
yuanagain <yuanw@princeton.edu>
2015-11-10 17:35:38 -0500

    sign up built, tab bar build

---
+++ BlueAgent2/playerDataManagement.swift
@@ -0,0 +1,68 @@
+//
+//  playerDataManagement.swift
+//  BlueAgent2
+//
+//  Created by W on 11/5/15.
+//  Copyright © 2015 BlueAgent. All rights reserved.
+//
+
+import Foundation
+
+class AparsePlayerController {
+    var name = "name"
+    var age = "age"
+    
+    
+    func load() {
+        
+    }
+    // array of tournaments
+    
+    // profile picture
+    
+    // load user data
+    
+    // User: profile picture
+    //
+    
+    // update data
+    
+    // recursive, so you should not point to other players, just their username?
+    
+}
+
+
+class AplayerController: NSObject {
+    // array of tournaments
+    
+    // profile picture
+    
+    // load user data
+    
+    // User: profile picture
+    //
+    
+    // update data
+    
+    // recursive, so you should not point to other players, just their username?
+    
+}
+
+
+//PFObject *gameScore = [PFObject objectWithClassName:@"GameScore"];
+//gameScore[@"score"] = @1337;
+//gameScore[@"playerName"] = @"Sean Plott";
+//gameScore[@"cheatMode"] = @NO;
+//[gameScore saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+//if (succeeded) {
+//// The object has been saved.
+//} else {
+//// There was a problem, check error.description
+//}
+//}];
+
+//var gameScore = PFObject(className:"GameScore")
+//gameScore["score"] = 1337
+//gameScore["playerName"] = "Sean Plott"
+//gameScore["cheatMode"] = false
+//gameScore.saveEventually()
