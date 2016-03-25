6a57e9caedc8fb16ef879868078aafe9d2583049
yuanagain <yuanw@princeton.edu>
2016-03-24 20:25:15 -0400

    adding styles and assets for global usage

---
+++ src/BlueSport/styles/customvals.js
@@ -0,0 +1,16 @@
+/*
+This module contains standard custom styling data for all apps
+*/
+'use strict';
+var React = require('react-native');
+
+const _cvals = {
+  mainfont: 'avenir',
+  skorange: '#F5A623',
+  skblue: '#4A90E2',
+  headerTextSize: 24,
+  normalTextSize: 20,
+  detailTextSize: 14,
+}
+
+module.exports = _cvals;
