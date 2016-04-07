1843dde95bce768e7a28d9ec490e98b48e937faf
elisegeoris <elisegeoris@yahoo.com>
2016-04-07 15:32:01 -0400

    Converting JS style sheets to CSS for easy inclusion

--- web_code/app/main.js
+++ web_code/app/main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {render} from 'react-dom';
-import Greeter from './Greeter';
+import Newsfeed from './newsfeed';
 
 import './main.css';
 
-render(<Greeter />, document.getElementById('root'));
+render(<Newsfeed />, document.getElementById('root'));
