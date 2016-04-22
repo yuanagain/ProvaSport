d825cd904566297a070147d73d4181c353cad473
lukas <llbrower@princeton.edu>
2016-04-22 18:16:09 -0400

    Started bracket page

--- app/main.js
+++ app/main.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReadDOM from 'react-dom'
 import './menu_styles.css';
 import Menu from './menu';
-//import Newsfeed from './newsfeed';
+import Newsfeed from './newsfeed';
 import Profile from './profile';
+import Bracket from './bracket'
 
 
 
-ReactDOM.render(<Profile/>, document.getElementById('root'));
+ReactDOM.render(<Bracket/>, document.getElementById('root'));
 
